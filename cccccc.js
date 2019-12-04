ac8fafdd929ee0e4ba2d07a4a3b22cf96e2c98b0
tylergrant90 <58467649+tylergrant90@users.noreply.github.com>
2019-12-04 17:09:38 +0700

    Add files via upload

---
+++ cccccc.js
@@ -0,0 +1,51 @@
+<!--
+
+/*
+*/
+
+function random_imglink(){
+var myimages=new Array()
+//
+myimages[1]="https://1.bp.blogspot.com/-DIy0yAdDnLk/XeJxAmaWeuI/AAAAAAAAAHQ/nKQc63izsZs47bfbIwyxikEqMvf2pHw4ACLcBGAsYHQ/s1600/10_892.jpg"
+myimages[2]="https://1.bp.blogspot.com/-MDxD3YFuWKA/XeJxAjsSGDI/AAAAAAAAAHY/ANoNkTwpI08jOXncCKPY-GpMvzZkxcAvACLcBGAsYHQ/s1600/110120.jpg"
+myimages[3]="https://1.bp.blogspot.com/-anuGt9-6BdI/XeJxGDHeWOI/AAAAAAAAAIY/dx8U7woqMSUi5kexahs0gJ16rW7KJXGZACLcBGAsYHQ/s1600/223.jpg"
+myimages[4]="https://1.bp.blogspot.com/-Y8cE8HjKmu4/XeJxIao-vuI/AAAAAAAAAI8/kRZuoXK3hOwjci7i9V5LylW9i6ByRHe5ACLcBGAsYHQ/s1600/466.jpg"
+myimages[5]="https://1.bp.blogspot.com/-92xVkh4jHEU/XeJxIp_bpJI/AAAAAAAAAJA/mC9WXw4aecctf1CAQslyZUCqcxBBEU9VwCLcBGAsYHQ/s1600/512.jpg"
+myimages[6]="https://1.bp.blogspot.com/-rj7rZ7sBB5Y/XeJxJNvAlMI/AAAAAAAAAJE/94efKbe-QS8qfYbXJ0yNuuu9AMHpctzagCLcBGAsYHQ/s1600/857.jpg"
+myimages[7]="https://1.bp.blogspot.com/-piuXyqAD2PA/XeJxCDiKgkI/AAAAAAAAAHo/9dtryXSldnkCn5MO0T3XDTy0QRBk5GJhgCLcBGAsYHQ/s1600/11981.jpg"
+myimages[8]="https://1.bp.blogspot.com/-cvMu2_wbpEs/XeJxEjsBBrI/AAAAAAAAAII/EnMSnow50UUDx4MX74CTEzKeh-f1i1R1QCLcBGAsYHQ/s1600/15295.jpg"
+myimages[9]="https://1.bp.blogspot.com/-GW3zg8sIjTU/XeJxElR-IOI/AAAAAAAAAIM/1LdHtJN4i6YAx-u9upvH95UJTip6T7VfgCLcBGAsYHQ/s1600/22284.jpg"
+myimages[10]="https://1.bp.blogspot.com/-p9vWQTG8AIM/XeJxGwwrVSI/AAAAAAAAAIk/cL7KAtJEIYg7K3DK0B9VHrqwoP5eOJHDgCLcBGAsYHQ/s1600/30125.jpg"
+myimages[11]="https://1.bp.blogspot.com/-IEhAaLhgm0Q/XeJxH_dmhvI/AAAAAAAAAI0/o89SiG7OGtgC7NQymDzx8HZFtO4QH4klACLcBGAsYHQ/s1600/35884.jpg"
+myimages[12]="https://1.bp.blogspot.com/-jM5vybbKcB8/XeJxHtNIVbI/AAAAAAAAAIs/mwqA4YbS8gcC0z7KsD3Swm_RXVfCeYZ4gCLcBGAsYHQ/s1600/33732.jpg"
+myimages[13]="https://1.bp.blogspot.com/-v35qXlUpk9w/XeJxJBvY9RI/AAAAAAAAAJI/T-fq9QKQl8wU5-r2K-7fb6n8s69Vkn6ZgCLcBGAsYHQ/s1600/99530.jpg"
+myimages[14]="https://1.bp.blogspot.com/-rtFwIskiwAA/XeJxBJoNZsI/AAAAAAAAAHc/xhOJenMor8YgGGKYNPlhoIQwZ8AvGm17wCLcBGAsYHQ/s1600/111402.jpg"
+myimages[15]="https://1.bp.blogspot.com/-tSCN2B4Q4oo/XeJxBj6P6iI/AAAAAAAAAHk/N9FjzYuNjWwNpnUhblIGAhlJkfOmdJczgCLcBGAsYHQ/s1600/114271.jpg"
+myimages[16]="https://1.bp.blogspot.com/-bkXKHyM_5o8/XeJxCrC-c0I/AAAAAAAAAHw/kRHRsYrvT84wA5nb45d8VsLz9kpvylRpgCLcBGAsYHQ/s1600/120823.jpg"
+myimages[17]="https://1.bp.blogspot.com/-sy7YPIDA5QE/XeJxCk3H9BI/AAAAAAAAAH0/LSNJmSyq23Y5SznWt004CyAZ1N4Cn4r9wCLcBGAsYHQ/s1600/122024.jpg"
+myimages[18]="https://1.bp.blogspot.com/-nYQ_-lFs2Zg/XeJxDNZMflI/AAAAAAAAAH8/DlRlJIZ96woDgmr8qfnSc0d1nEDvnWsEgCLcBGAsYHQ/s1600/130154.jpg"
+myimages[19]="https://1.bp.blogspot.com/-rPCx-GiaFvQ/XeJxDDAbwZI/AAAAAAAAAIA/cr35z6aRcRYPE60vPNU7SLo8FHDAAj3jwCLcBGAsYHQ/s1600/131092.jpg"
+myimages[20]="https://1.bp.blogspot.com/--UU86E7kiIg/XeJxDyhyUXI/AAAAAAAAAIE/JB5Mb6ozFHgyFo_NH6_7iYdIDojWPmnRQCLcBGAsYHQ/s1600/145544.jpg"
+myimages[21]="https://1.bp.blogspot.com/-SPyNEia-6Qg/XeJxGNzrIKI/AAAAAAAAAIU/8U0VtPe71P4t1hRR5KVhWA59saTmeYgzQCLcBGAsYHQ/s1600/251161.jpg"
+myimages[22]="https://1.bp.blogspot.com/-Wg7OcW24atU/XeJxGAWo3AI/AAAAAAAAAIc/YyFOuWaHpUwdnsRB87lZQ1nvdgx-vqtRwCLcBGAsYHQ/s1600/252161.jpg"
+myimages[23]="https://1.bp.blogspot.com/-3r2r_tIdGrU/XeJxG2MgtPI/AAAAAAAAAIg/PGUs_lZ37k4LeP1qFA5fk6txYXURn_HJACLcBGAsYHQ/s1600/252600.jpg"
+myimages[24]="https://1.bp.blogspot.com/-KyZCoMEaxsE/XeJxHeiC6WI/AAAAAAAAAIo/DwSUCxSPx7ULe-SPH-SNaLw4CAcphsSBACLcBGAsYHQ/s1600/328400.jpg"
+myimages[25]="https://1.bp.blogspot.com/-Yhull9lslcg/XeJxHswak1I/AAAAAAAAAIw/PAdG6jHhGvAbE1P17eGNC0-aIkg1uUDpQCLcBGAsYHQ/s1600/330153.jpg"
+myimages[26]="https://1.bp.blogspot.com/-7rC0Tl15QjM/XeJxIMAL1BI/AAAAAAAAAI4/lHGX6X5IKdoWm03yIDfx-eKf3-2br2VKQCLcBGAsYHQ/s1600/378458.jpg"
+myimages[27]="https://1.bp.blogspot.com/-eHFsAk0Td30/XeJxAjaPTXI/AAAAAAAAAHU/rDZsXo46RKUeUV420So5uowDlCjo27xBgCLcBGAsYHQ/s1600/1078573.jpg"
+myimages[28]="https://1.bp.blogspot.com/-dSwtWuWe_oM/XeJxBrAscOI/AAAAAAAAAHg/P8FHLmspLOgRTcCPnGsje9DTVM9uNGxJgCLcBGAsYHQ/s1600/1189560.jpg"
+myimages[29]="https://1.bp.blogspot.com/-43XFQ5LBwFc/XeJxCd7oH-I/AAAAAAAAAHs/sKDzqzFvAyEkGxtJ8t7wUQC6Gz-cgS2uQCLcBGAsYHQ/s1600/1213403.jpg"
+myimages[30]="https://1.bp.blogspot.com/-ass0CUhcqR0/XeJxDC3goKI/AAAAAAAAAH4/7QYvobBk9I0RU7ZdZIBwaIRCNBjKOPZEQCLcBGAsYHQ/s1600/1233306.jpg"
+myimages[31]="https://1.bp.blogspot.com/-Z_MMp-jqLiQ/XeJxEpou-9I/AAAAAAAAAIQ/KA2wZdGZKfs0fJ5SlPzzVd7yfKs8t7jcgCLcBGAsYHQ/s1600/1561.jpg"
+
+//
+var imagelinks=new Array()
+imagelinks[0]="http://www.google.com"
+
+var ry=Math.floor(Math.random()*myimages.length)
+if (ry==0)
+ry=1
+document.write('<a href='+'"'+imagelinks[ry]+'"'+'><img src="'+myimages[ry]+'" border=0 width=525 height=600></a>')
+}
+random_imglink()
+//-->
